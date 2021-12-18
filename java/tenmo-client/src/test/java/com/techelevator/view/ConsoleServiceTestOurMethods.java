e61b0e36e84e2a6ab82774aea36f91bee0bd9577
Ryan Goodchild <ryang2320@gmail.com>
2021-12-18 15:13:40 -0500

    Pushing for safety before putting on the lab goggles

--- java/tenmo-client/src/test/java/com/techelevator/view/ConsoleServiceTestOurMethods.java
+++ java/tenmo-client/src/test/java/com/techelevator/view/ConsoleServiceTestOurMethods.java
@@ -1,6 +1,9 @@
 package com.techelevator.view;
 
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.TransferDTO;
 import com.techelevator.tenmo.model.User;
+import io.cucumber.java.bs.A;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,32 +26,27 @@ public class ConsoleServiceTestOurMethods {
     }
     //**********************
 
-    @Test
-    public void presentBalance() {
-    }
 
     @Test
-    public void presentUserList() {
-    }
-
-    @Test
-    public void presentTransferList() {
-    }
-
-    @Test
-    public void presentChosenTransfer() {
-    }
-
-    @Test
-    public void messageToUser() {
-    }
-
-    @Test
-    public void Returns_User_Input_BigDecimal() {
+    public void Returns_User_Input_BigDecimal_Success_When_Valid_Input() {
         BigDecimal expected = new BigDecimal(30.15);
         ConsoleService console = getServiceForTestingWithUserInput(expected.toString());
         BigDecimal result = console.getUserInputBigDecimal("30.15");
         Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void Returns_User_Input_BigDecimal_Fails_When_INVALID_Input() {
+        ConsoleService console = getServiceForTestingWithUserInput("Noway");
+
+        String actual = "";
+        BigDecimal result;
+        try {
+            result = console.getUserInputBigDecimal("20.35");
+        } catch (NoSuchElementException e) {
+            actual = "failed";
+        }
+        Assert.assertEquals("failed", actual);
     }
 
     @Test
@@ -59,16 +57,9 @@ public class ConsoleServiceTestOurMethods {
         Assert.assertEquals(expected, result);
     }
 
-//    @Test
-//    public void returns_user_input_for_integer() {
-//        Integer expected = 27;
-//        ConsoleService console = getServiceForTestingWithUserInput(expected.toString());
-//        Integer result = console.getUserInputInteger("Enter a number");
-//        Assert.assertEquals(expected, result);
-//    }
 
     @Test
-    public void Get_Amount_Returns_Correct_Amount_ID_Is_Found() {
+    public void Get_Amount_Returns_Correct_ID_When_ID_Is_Found() {
         User testUser = new User();
         testUser.setUsername("bob");
         testUser.setId(3);
@@ -87,7 +78,7 @@ public class ConsoleServiceTestOurMethods {
     }
 
     @Test
-    public void Get_Amount_Returns_Correct_Amount_ID_Is_NOT_Found() {
+    public void Get_Amount_Returns_Nothing_ID_Is_NOT_Found() {
         User testUser = new User();
         testUser.setUsername("bob");
         testUser.setId(3);
@@ -102,29 +93,90 @@ public class ConsoleServiceTestOurMethods {
         String actual = "";
         Integer result;
         try {
-             result = console.getAmount(userArray, testUser.getId());
+            result = console.getAmount(userArray, testUser.getId());
         } catch (NoSuchElementException e) {
-             actual = "failed";
+            actual = "failed";
         }
- Assert.assertEquals("failed", actual);
+        Assert.assertEquals("failed", actual);
 
 
-//        Integer expectedOne = testUser.getId();
-//        Integer expectedTwo = secondTestUser.getId();
-//        //Assert.assertEquals("", result);
-//        // Assert.fail();
-//        Assert.assertNotSame(expectedOne, result);
-//        Assert.assertNotSame(expectedTwo, result);
     }
 
     @Test
-    public void checkTransferAmount() {
+    public void Check_Transfer_Amount_Send_Amount_Is_Less_Than_Balance_So_Can_Send() {
+        Account account = new Account();
+        account.setAccountBalance(new BigDecimal("20.50"));
+
+        BigDecimal expected = new BigDecimal("20.00");
+
+        ConsoleService console = getServiceForTestingWithUserInput("20.00");
+        BigDecimal actual = (console.checkTransferAmount(account));
+
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void checkTransferId() {
+    public void Check_Transfer_Amount_Send_Amount_Is_More_Than_Balance_So_CANNOT_Send() {
+        Account account = new Account();
+        account.setAccountBalance(new BigDecimal("18.50"));
+
+        ConsoleService console = getServiceForTestingWithUserInput("20.00");
+
+        BigDecimal result = new BigDecimal("0");
+        String actual = "";
+        try {
+            result = console.checkTransferAmount(account);
+        } catch (NoSuchElementException e) {
+            actual = "failed";
+        }
+        Assert.assertEquals("failed", actual);
     }
 
+    @Test
+    public void Check_Transfer_Id_Given_Correct_Id_Returns_Transfer_Id() {
+
+        TransferDTO transOne = new TransferDTO();
+        transOne.setTransferId(1);
+        TransferDTO transTwo = new TransferDTO();
+        transTwo.setTransferId(2);
+        TransferDTO transThree = new TransferDTO();
+        transThree.setTransferId(3);
+
+        TransferDTO[] transfers = {transOne, transTwo, transThree};
+
+        ConsoleService console = getServiceForTestingWithUserInput("2");
+        Integer result = console.checkTransferId(transfers, 1);
+        Integer expected = 2;
+
+        Assert.assertEquals(expected, result);
+
+    }
+
+    @Test
+    public void Check_Transfer_Id_Given_Non_Existent_Id_Returns_No_Element_Exception() {
+
+        TransferDTO transOne = new TransferDTO();
+        transOne.setTransferId(1);
+        TransferDTO transTwo = new TransferDTO();
+        transTwo.setTransferId(2);
+        TransferDTO transThree = new TransferDTO();
+        transThree.setTransferId(3);
+
+        TransferDTO[] transfers = {transOne, transTwo, transThree};
+
+        ConsoleService console = getServiceForTestingWithUserInput("404");
+
+        String actual = "";
+        try {
+            Integer result = console.checkTransferId(transfers, 1);
+        } catch (NoSuchElementException e) {
+            actual = "failed";
+        }
+        Assert.assertEquals("failed", actual);
+
+    }
+
+    //******************************************************************************
     private ConsoleService getServiceForTestingWithUserInput(String userInput) {
         ByteArrayInputStream input = new ByteArrayInputStream(String.valueOf(userInput).getBytes());
         return new ConsoleService(input, output);
